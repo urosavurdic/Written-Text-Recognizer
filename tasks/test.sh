45a116c7483278d49e65180b935a2ccbed683b97
Uros Savurdic <savurdic.uros@gmail.com>
2025-10-04 01:10:52 +0200

    cicd

---
+++ tasks/test.sh
@@ -0,0 +1,14 @@
+set -uo pipefail
+set +e
+
+FAILURE=false
+
+./training/tests/test_run_training.sh || FAILURE=true
+pytest -s . || FAILURE=true
+
+if [ "$FAILURE" = true ]; then
+  echo "Tests failed"
+  exit 1
+fi
+echo "Tests passed"
+exit 0
